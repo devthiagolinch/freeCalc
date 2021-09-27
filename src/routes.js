b2d5bdf7fd06dd4ea89fd8e707e47fa3f3226fed
Thiago Linch <devthiagolinch@gmail.com>
2021-09-27 08:23:42 -0400

    aula02 Remaining calculation

--- src/routes.js
+++ src/routes.js
@@ -9,24 +9,57 @@ const profile = {
     "days-per-week": "5",
     "hours-per-day": "5",
     "vacation-per-year": "4",
+    "value-hour": 75
 };
 
 const jobs = [
     {
         id: "1",
         name: "Pizzario Guloso",
-        "daily-hours": "3",
-        "total-hours": "60",
+        "daily-hours": 2,
+        "total-hours": 60,
         createdAt: Date.now(),
     },
     {
         id: "2",
         name: "oneTwo Projects",
-        "daily-hours": "4",
-        "total-hours": "30",
+        "daily-hours": 3,
+        "total-hours": 47,
         createdAt: Date.now(),
     }
 ]
+
+// function para calculo de time dos projetos
+function restingDays(job) {
+     // ajustes do jobs
+        // calculo de tempo restante
+        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() //toFixed fucniona para "arredondar" o valor da divisao
+        
+        // Para ter o dia exato de creat do job
+        // function que ira fazer os milissegundos do createdAt se tornarem data real
+        const createdDate = new Date(job.createdAt)
+        
+        // Tempo de entrega
+        // getDate = o dia do mes exato | Number() para transformar o remainingDays em numero novamente
+        // conta sera do dia do mes que estamos + quantos dias que restam para a entrega = dia de entrega
+        const dueDay = createdDate.getDate() + Number(remainingDays)
+
+        // Criando a data do futuro = data de vencimetno do projeto
+        // setDate = nova data
+        const dueDate = createdDate.setDate(dueDay)
+
+        // diferenca do tempo em milissegundos para dias restantes do projeto
+        // tudo esta em milissegundos, entao preciso passar para dia em dayDiff divido por DayinMs
+        const timeDiffInMs = dueDate - Date.now()
+        // transformar Ms em Dias
+        // aqui temos quando Ms temos em um dia para transformar o timeDiffInMs total em days
+        const dayInMs = 1000 * 60 * 60 *24
+        // Math.floor para transformar tudo em number e arredondar para baixo | .toFixed() arredonda para cima
+        const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+
+        //restam x dias
+        return dayDiff
+}
 
 
 // Create a base route for routes
@@ -43,25 +76,19 @@ const views = __dirname + "/views/";
 
 routes.get('/', (req, res) =>  {
     const updateJobs = jobs.map((job) => {
-         // ajustes do jobs
-        // calculo de tempo restante
-        const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed() //toFixed fucniona para "arredondar" o valor da divisao
-        
-        // Para ter o dia exato de creat do job
-        // function que ira fazer os milissegundos do createdAt se tornarem data real
-        const createdDate = new Date(job.createdAt)
-        
-        // Tempo de entrega
-        // getDate = o dia do mes exato | Number() para transformar o remainingDays em numero novamente
-        // conta sera do dia do mes que estamos + quantos dias que restam para a entrega = dia de entrega
-        const dueDay = createdDate.getDate() + Number(remainingDays)
-        const dueDate = createDate.setDate(dueDay)
+        // ajustes do job
+        const remaining = restingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
 
-
-        return job
+        return {
+            ...job, // espalhamento JS, pegar os dados e jogar aqui dentro
+            remaining,
+            status,
+            budget: profile['value-hour'] * job['total-hours']
+        }
     })
 
-    res.render(views + "index", {jobs})
+    res.render(views + "index", { jobs:updateJobs })
 })
 
 
